2d4e83bffc7ea45fd8af8478035d05b92a8fbebb
Mr.Fish <martinez.facundo85@gmail.com>
2023-09-24 19:25:26 -0300

    Inicio del proyecto

---
+++ src/main/java/edu/unam/webbapp/consultorio/WebbappConsultorioApplication.java
@@ -0,0 +1,13 @@
+package edu.unam.webbapp.consultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbappConsultorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbappConsultorioApplication.class, args);
+	}
+
+}
