536f58585c98773e8f4c95d5798f41788348091e
Mr.Fish <martinez.facundo85@gmail.com>
2023-10-16 21:57:16 -0300

    add: documentacion para enums

--- src/main/java/edu/unam/webbapp/consultorio/utils/EstadoCivil.java
+++ src/main/java/edu/unam/webbapp/consultorio/utils/EstadoCivil.java
@@ -1,5 +1,9 @@
 package edu.unam.webbapp.consultorio.utils;
 
+/**
+ * Clase Persona (clase padre)
+ * @author Andre,Gelabert; Pavon, Gabriel; Martinez, Facundo
+ */
 public enum EstadoCivil {
     CASADO,
     SOLTERO,
