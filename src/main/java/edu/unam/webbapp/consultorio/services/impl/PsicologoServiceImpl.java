e901f289af38e84d80309548db8243a2619f7867
yellowDuck23 <diacono857@gmail.com>
2023-10-26 18:45:55 -0300

    add: documentation

--- src/main/java/edu/unam/webbapp/consultorio/services/impl/PsicologoServiceImpl.java
+++ src/main/java/edu/unam/webbapp/consultorio/services/impl/PsicologoServiceImpl.java
@@ -8,31 +8,63 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Clase PsicologoServiceImpl
+ * @author Andre,Gelabert; Pavon, Gabriel; Martinez, Facundo
+ */
 @Service
 @RequiredArgsConstructor
 public class PsicologoServiceImpl implements PersonaService<Psicologo> {
 
   private final PsicologoRepository repo;
 
+  /**
+   * Funcion que retorna una lista de psicologos
+   * @return lista de psicologos
+   */
   @Override
   public List<Psicologo> findAll() {
     return repo.findAll();
   }
 
+  /**
+   * Funcion que retorna un psicologo a partir de su numero de identificacion
+   * @param id numero de identificacion
+   * @return un psicologo
+   */
   @Override
   public Psicologo getById(Integer id) {
     return repo.findById(id).orElseThrow();
   }
+
+  /**
+   * Funcion que retorna un psicologo y lo persiste
+   * @param psicologo un objeto psicologo
+   * @return un objeto psicologo
+   */
 
   @Override
   public Psicologo save(Psicologo psicologo) {
     return repo.save(psicologo);
   }
 
+  /**
+   * Funcion que sirve para actualizar un psicologo determinado a partir de su ID
+   * @param id número de indentificación del psicologo
+   * @param psicologo un objeto psicologo
+   * @return un objeto psicologo
+   * @deprecated
+   */
+
   @Override
   public Psicologo update(Integer id, Psicologo psicologo) {
     return null;
   }
+
+  /**
+   * Funcion que elimina logicamente un psicologo mediante su numero de identificacion
+   * @param id numero de identificación
+   */
 
   @Override
   public void deleteById(Integer id) {
--- src/main/java/edu/unam/webbapp/consultorio/services/impl/SecretarioServiceImpl.java
+++ src/main/java/edu/unam/webbapp/consultorio/services/impl/SecretarioServiceImpl.java
@@ -8,31 +8,64 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Clase SecretarioServiceImpl
+ * @author Andre,Gelabert; Pavon, Gabriel; Martinez, Facundo
+ */
 @Service
 @RequiredArgsConstructor
 public class SecretarioServiceImpl implements PersonaService<Secretario> {
 
     private final SecretarioRepository repo;
 
+    /**
+     * Funcion que lista todos los secretarios disponibles
+     * @return lista de secretarios
+     */
     @Override
     public List<Secretario> findAll() {
     return repo.findAll();
     }
 
+    /**
+     * Funcion que busca un secretario a partir de su numero de identificacion
+     * @param id numero de identificacion
+     * @return un objeto secretario
+     */
     @Override
     public Secretario getById(Integer id) {
         return repo.findById(id).orElseThrow();
     }
 
+    /**
+     * Funcion que sirve para persistir un objeto secretario
+     * @param secretario un objeto secretario
+     * @return objeto secretario
+     */
     @Override
     public Secretario save(Secretario secretario) {
         return repo.save(secretario);
     }
 
+    /**
+     * Funcion que sirve para actualizar un secretario a partir de su numero de identificacion
+     * @param id número de indentificación
+     * @param secretario un objeto secretario
+     * @return objeto secreatario
+     * @deprecated
+     */
     @Override
     public Secretario update(Integer id, Secretario secretario) {
         return null;
     }
+
+    /**
+     * Funcion que elimina logicamente un secretario.<br>
+     * Verifica si el secretario se encuentra disponible.
+     * Una vez que lo encuentra configura el setEliminado en TRUE y
+     * luego persiste el objeto modificado.
+     * @param id numero de identificación
+     */
 
     @Override
     public void deleteById(Integer id) {
