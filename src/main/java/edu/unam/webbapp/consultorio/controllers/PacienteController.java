c539430c4a1b82f10307bca91e3692fa3c891886
facundo1000 <martinez.facundo85@gmail.com>
2023-10-30 20:11:30 -0300

    add: actualizacion ABM pacientes

--- src/main/java/edu/unam/webbapp/consultorio/controllers/PacienteController.java
+++ src/main/java/edu/unam/webbapp/consultorio/controllers/PacienteController.java
@@ -3,7 +3,6 @@ package edu.unam.webbapp.consultorio.controllers;
 import edu.unam.webbapp.consultorio.model.Paciente;
 import edu.unam.webbapp.consultorio.model.Psicologo;
 import edu.unam.webbapp.consultorio.services.PersonaService;
-import edu.unam.webbapp.consultorio.services.impl.PacienteServiceImpl;
 import edu.unam.webbapp.consultorio.utils.EstadoCivil;
 import edu.unam.webbapp.consultorio.utils.Sexo;
 import edu.unam.webbapp.consultorio.utils.TipoDocumento;
@@ -14,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
+
 /**
  * Clase PacienteController
  * @author Andre,Gelabert; Pavon, Gabriel; Martinez, Facundo
@@ -23,94 +23,79 @@ import org.springframework.web.bind.support.SessionStatus;
 @SessionAttributes("paciente")
 public class PacienteController {
 
-  private final PersonaService<Paciente> service;
-  private final PersonaService<Psicologo> psicoService;
+    private final PersonaService<Paciente> service;
+    private final PersonaService<Psicologo> psicoService;
 
-  @GetMapping("/lista-pacientes")
-  public String mostrar(Model model) {
-//    model.addAttribute("titulo", "Listado de Pacientes");
-    model.addAttribute("pacientes", service.findAll());
-    Paciente paciente = new Paciente();
-    Psicologo psicologo = new Psicologo();
-//    model.addAttribute("titulo", "Registro de Paciente");
-    model.addAttribute("tipos", TipoDocumento.values());
-    model.addAttribute("sexos", Sexo.values());
-    model.addAttribute("estados", EstadoCivil.values());
-    model.addAttribute("paciente", paciente);
-    model.addAttribute("psicologo", psicologo);
-    model.addAttribute("psicologos", psicoService.findAll());
-    return "pacientes/abmPaciente";
-  }
+    @GetMapping("/abm-pacientes")
+    public String CrearMostrar(Model model) {
 
-  /*
-  @GetMapping("/form-paciente")
-  public String crear(Model model) {
-    Paciente paciente = new Paciente();
-    Psicologo psicologo = new Psicologo();
-    model.addAttribute("titulo", "Registro de Paciente");
-    model.addAttribute("tipos", TipoDocumento.values());
-    model.addAttribute("sexos", Sexo.values());
-    model.addAttribute("estados", EstadoCivil.values());
-    model.addAttribute("paciente", paciente);
-    model.addAttribute("psicologo", psicologo);
-    model.addAttribute("psicologos", psicoService.findAll());
-    return "pacientes/formPaciente";
-  }
-
-   */
-
-  @PostMapping("/form-paciente")
-  public String guardar(
-      @RequestParam("psicologo") Integer psicoDni,
-      @Valid Paciente paciente,
-      BindingResult result,
-      Model model,
-      SessionStatus status) {
-
-    if (result.hasErrors()) {
-      model.addAttribute("titulo", "Registro de Paciente");
-      model.addAttribute("tipos", TipoDocumento.values());
-      model.addAttribute("sexos", Sexo.values());
-      model.addAttribute("estados", EstadoCivil.values());
-      model.addAttribute("psicologos", psicoService.findAll());
-      return "pacientes/formPaciente";
+        model.addAttribute("titulo", "Registrar nuevo paciente");
+        Paciente paciente = new Paciente();
+        Psicologo psicologo = new Psicologo();
+        model.addAttribute("pacientes", service.findAll());
+        model.addAttribute("tipos", TipoDocumento.values());
+        model.addAttribute("sexos", Sexo.values());
+        model.addAttribute("estados", EstadoCivil.values());
+        model.addAttribute("paciente", paciente);
+        model.addAttribute("psicologo", psicologo);
+        model.addAttribute("psicologos", psicoService.findAll());
+        return "pacientes/abmPaciente";
     }
 
-    Psicologo psicologo = psicoService.getById(psicoDni);
-    paciente.setPsicologo(psicologo);
-    paciente.setEliminado(Boolean.FALSE);
-    psicologo.addPaciente(paciente);
-    psicoService.save(psicologo);
+    @PostMapping("/abm-pacientes")
+    public String guardar(
+            @RequestParam("psicologo") Integer psicoDni,
+            @Valid Paciente paciente,
+            BindingResult result,
+            Model model,
+            SessionStatus status) {
 
-    service.save(paciente);
-    status.setComplete();
-    return "redirect:/lista-pacientes";
-  }
+        if (result.hasErrors()) {
+            model.addAttribute("pacientes", service.findAll());
+            model.addAttribute("titulo", "Registrar nuevo paciente");
+            model.addAttribute("tipos", TipoDocumento.values());
+            model.addAttribute("sexos", Sexo.values());
+            model.addAttribute("estados", EstadoCivil.values());
+            model.addAttribute("psicologos", psicoService.findAll());
+            return "pacientes/abmPaciente";
+        }
 
-  @GetMapping("/form-paciente/{id}")
-  public String editar(@PathVariable("id") Integer id, Model model) {
-    Paciente paciente;
+        Psicologo psicologo = psicoService.getById(psicoDni);
+        paciente.setPsicologo(psicologo);
+        paciente.setEliminado(Boolean.FALSE);
+        psicologo.addPaciente(paciente);
+        psicoService.save(psicologo);
 
-    if (id > 0) {
-      paciente = service.getById(id);
-      model.addAttribute("tipos", TipoDocumento.values());
-      model.addAttribute("sexos", Sexo.values());
-      model.addAttribute("estados", EstadoCivil.values());
-      model.addAttribute("psicologos", psicoService.findAll());
-      model.addAttribute("pacientes", service.findAll());
-    } else {
-      return "redirect:/lista-pacientes";
+        service.save(paciente);
+        status.setComplete();
+        return "redirect:/abm-pacientes";
     }
 
-    model.addAttribute("paciente", paciente);
-    return "pacientes/abmPaciente";
-  }
+    @GetMapping("/abm-pacientes/{id}")
+    public String editar(@PathVariable("id") Integer id, Model model) {
+        Paciente paciente;
 
-  @GetMapping("/eliminar-paciente/{id}")
-  public String eliminar(@PathVariable("id") Integer id) {
-    if (id > 0) {
-      service.deleteById(id);
+        if (id > 0) {
+            paciente = service.getById(id);
+            model.addAttribute("pacientes", service.findAll());
+            model.addAttribute("tipos", TipoDocumento.values());
+            model.addAttribute("sexos", Sexo.values());
+            model.addAttribute("estados", EstadoCivil.values());
+            model.addAttribute("psicologos", psicoService.findAll());
+        } else {
+            return "redirect:/abm-pacientes";
+        }
+
+        model.addAttribute("titulo", "Editando el Paciente");
+        model.addAttribute("paciente", paciente);
+        return "pacientes/abmPaciente";
     }
-    return "redirect:/lista-pacientes";
-  }
+
+    @GetMapping("/eliminar-paciente/{id}")
+    public String eliminar(@PathVariable("id") Integer id) {
+        if (id > 0) {
+            service.deleteById(id);
+        }
+        return "redirect:/abm-paciente";
+    }
 }
