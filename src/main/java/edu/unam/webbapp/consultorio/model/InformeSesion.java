8b910c5a6a2a64e830573d27e3248f18f8f35d1f
facundo1000 <martinez.facundo85@gmail.com>
2023-11-06 08:05:29 -0300

    add: nuevos atributos a informe

--- src/main/java/edu/unam/webbapp/consultorio/model/InformeSesion.java
+++ src/main/java/edu/unam/webbapp/consultorio/model/InformeSesion.java
@@ -2,29 +2,30 @@ package edu.unam.webbapp.consultorio.model;
 
 import edu.unam.webbapp.consultorio.utils.TipoDeDescripcion;
 import jakarta.persistence.*;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Setter
 @Getter
 @NoArgsConstructor
-@Table(name = "informeSesion")
+@Table(name = "informe_sesion")
 public class InformeSesion {
 
     //TODO: Solucionar validacion con id.
     //Identificador unico de un informe
     @Id
     @NotNull
-    @Min(9)
+    @Column(name = "id_informe")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idInforme;
 
     //Fecha de la sesion
@@ -37,28 +38,38 @@ public class InformeSesion {
     @Column(name = "hora_informe")
     @PastOrPresent
     @NotNull
+    @DateTimeFormat(pattern = "HH:mm")
     private LocalTime horaSesion;
 
 
     @NotEmpty
-    private String descripcion;
+    @Column(nullable = false)
+    private String analisis;
 
-    @Enumerated(EnumType.STRING)
-    private TipoDeDescripcion tipoDeDescripcion;
+    private String notas;
+
+    private String conclusion;
+
+    @OneToOne
+    private Sesion sesion;
+
+//    @Enumerated(EnumType.STRING)
+//    @Column(name = "tipo_descripcion")
+//    private TipoDeDescripcion tipoDeDescripcion;
 
     /**
      * <p>
-     *   Valor que justifica si la sesion a sido eliminada,
-     *   o no.
+     * Valor que justifica si la sesion a sido eliminada,
+     * o no.
      * </p>
      */
     private Boolean eliminado;
 
     /**
      * <p>
-     *  Metodo que configura por defecto
-     *  el valor de 'falso' antes que se persista
-     *  una entidad en una base de datos
+     * Metodo que configura por defecto
+     * el valor de 'falso' antes que se persista
+     * una entidad en una base de datos
      * </p>
      */
     @PrePersist
