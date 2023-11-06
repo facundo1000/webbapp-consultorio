e4825799039233e3dd96ce96ae178e702cdd86dd
facundo1000 <martinez.facundo85@gmail.com>
2023-11-06 08:03:15 -0300

    add: funciones de estados de sesion

--- src/main/java/edu/unam/webbapp/consultorio/services/impl/SesionServiceImpl.java
+++ src/main/java/edu/unam/webbapp/consultorio/services/impl/SesionServiceImpl.java
@@ -12,91 +12,105 @@ import java.util.List;
 import edu.unam.webbapp.consultorio.services.SesionService;
 import edu.unam.webbapp.consultorio.utils.EstadosSesion;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 /**
  * Clase SesionServiceImpl
- * @author Andre,Gelabert; Pavon, Gabriel; Martinez, Facundo
+ *
+ * @author Andre, Gelabert; Pavon, Gabriel; Martinez, Facundo
  */
 @Service
 @RequiredArgsConstructor
 public class SesionServiceImpl implements SesionService {
 
-  private final SesionRepositorio repo;
+    private final SesionRepositorio repo;
 
-  /**
-   * Función que retorna una lista de sesiones
-   * @return lista de sesiones
-   */
-  @Override
-  public List<Sesion> findAll() {
-    return repo.findAll();
-  }
-
-  /**
-   * Función que retorna un objeto sesion
-   * @param id número de identificación
-   * @return un objeto sesion
-   */
-
-  @Override
-  public Sesion findById(Integer id) {
-    return repo.findById(id).orElseThrow();
-  }
-
-  /**
-   * Función que retorna un objeto sesion
-   * @param sesion un objeto sesion
-   * @return un objeto sesion
-   */
-
-  @Override
-  public Sesion save(Sesion sesion) {
-    return repo.save(sesion);
-  }
-
-  /**
-   * Función que actualiza un objeto sesion
-   * @param id un número de identificación de sesion
-   * @param sesion un objeto sesion
-   * @return un objeto sesion modificado
-   */
-
-  @Override
-  public Sesion update(Integer id, Sesion sesion) {
-    return null;
-  }
-
-  /**
-   * Función que obtiene una lista de sesiones a partir de un paciente
-   * @param id número identificación de un paciente
-   * @return lista de sesiones por paciente
-   */
-  @Override
-  public List<Sesion> findSesionByPacienteId(Integer id) {
-    return repo.findSesionsByPacienteDni(id);
-  }
-
-  /**
-   * Función que elimina lógicamente una sesion
-   * @param id número de identificación de una sesion
-   */
-  @Override
-  public void deleteById(Integer id) {
-    Sesion sesion = repo.findById(id).orElseThrow();
-    sesion.setEliminado(true);
-  }
-
-  @Override
-  public Sesion sesionStatus(LocalDate fecha, Sesion sesion) {
-    if(fecha.isAfter(LocalDate.now())){
-      sesion.setEstadosSesion(EstadosSesion.PENDIENTE);
+    /**
+     * Función que retorna una lista de sesiones
+     *
+     * @return lista de sesiones
+     */
+    @Override
+    public List<Sesion> findAll() {
+        return repo.findAll();
     }
 
-    //Esto deberia llevar un metodo un metodo un metodo automatico que esté pendiente del horario del sistema
-    if(fecha.isBefore(LocalDate.now())){
-      sesion.setEstadosSesion(EstadosSesion.REALIZADA);
+    /**
+     * Función que retorna un objeto sesion
+     *
+     * @param id número de identificación
+     * @return un objeto sesion
+     */
+
+    @Override
+    public Sesion findById(Integer id) {
+        return repo.findById(id).orElseThrow();
     }
-    return sesion;
-  }
+
+    /**
+     * Función que retorna un objeto sesion
+     *
+     * @param sesion un objeto sesion
+     * @return un objeto sesion
+     */
+
+    @Override
+    public Sesion save(Sesion sesion) {
+        return repo.save(sesion);
+    }
+
+    /**
+     * Función que actualiza un objeto sesion
+     *
+     * @param id     un número de identificación de sesion
+     * @param sesion un objeto sesion
+     * @return un objeto sesion modificado
+     */
+
+    @Override
+    public Sesion update(Integer id, Sesion sesion) {
+        return null;
+    }
+
+    /**
+     * Función que obtiene una lista de sesiones a partir de un paciente
+     *
+     * @param id número identificación de un paciente
+     * @return lista de sesiones por paciente
+     */
+    @Override
+    public List<Sesion> findSesionByPacienteId(Integer id) {
+        return repo.findSesionsByPacienteDni(id);
+    }
+
+    /**
+     * Función que elimina lógicamente una sesion
+     *
+     * @param id número de identificación de una sesion
+     */
+    @Override
+    public void deleteById(Integer id) {
+        Sesion sesion = repo.findById(id).orElseThrow();
+        sesion.setEliminado(true);
+    }
+
+
+    @Override
+    public Sesion sesionStatus(LocalDate fecha, Sesion sesion) {
+        if (fecha.isAfter(LocalDate.now()) || fecha.isEqual(LocalDate.now())) {
+            sesion.setEstadosSesion(EstadosSesion.PENDIENTE);
+        }
+
+        /**
+         * TODO:
+         *  Esto deberia llevar un metodo un metodo un metodo automatico que esté pendiente del horario del sistema.
+         *  la realizacion se puede hacer de forma manual o de forma automatica
+         */
+
+        if (fecha.isBefore(LocalDate.now())) {
+            sesion.setEstadosSesion(EstadosSesion.REALIZADA);
+        }
+        return sesion;
+    }
 }
