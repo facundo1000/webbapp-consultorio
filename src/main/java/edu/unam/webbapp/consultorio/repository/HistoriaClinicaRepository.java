6fa79c77a95de255b89881eeac023f053518ccf0
facundo1000 <martinez.facundo85@gmail.com>
2023-11-20 20:13:36 -0300

    add: historiaClinica repository

---
+++ src/main/java/edu/unam/webbapp/consultorio/repository/HistoriaClinicaRepository.java
@@ -0,0 +1,9 @@
+package edu.unam.webbapp.consultorio.repository;
+
+import edu.unam.webbapp.consultorio.model.HistoriaClinica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistoriaClinicaRepository extends JpaRepository<HistoriaClinica,Integer> {
+
+
+}
