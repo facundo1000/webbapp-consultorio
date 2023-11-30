57ab029dbbf641460e0b11baac5a2b4571937bcc
facundo1000 <martinez.facundo85@gmail.com>
2023-11-30 18:10:08 -0300

    add: controller methods

--- src/main/java/edu/unam/webbapp/consultorio/controllers/HistoriaClinicaController.java
+++ src/main/java/edu/unam/webbapp/consultorio/controllers/HistoriaClinicaController.java
@@ -1,15 +1,19 @@
 package edu.unam.webbapp.consultorio.controllers;
 
 import edu.unam.webbapp.consultorio.model.HistoriaClinica;
-import edu.unam.webbapp.consultorio.model.InformeSesion;
+import edu.unam.webbapp.consultorio.model.Paciente;
+import edu.unam.webbapp.consultorio.model.Sesion;
 import edu.unam.webbapp.consultorio.services.HistoriaClinicaService;
+import edu.unam.webbapp.consultorio.services.PersonaService;
+import edu.unam.webbapp.consultorio.services.SesionService;
+import edu.unam.webbapp.consultorio.services.impl.PacienteServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Clase HistoriaClinicaController
@@ -22,13 +26,31 @@ public class HistoriaClinicaController {
 
     private final HistoriaClinicaService service;
 
+    private final PacienteServiceImpl servicPaciente;
+
+    private final SesionService sesionService;
 
     @GetMapping("/historia-clinica")
     public String mostrar(Model model) throws Exception {
         List<HistoriaClinica> historiaClinica = service.findAllHistoriaClinica();
-        Optional<List<InformeSesion>> sesions = historiaClinica.stream().map(HistoriaClinica::getInformeSesions).findFirst();
-        sesions.ifPresent(informeSesions -> model.addAttribute("informes", informeSesions));
-
+        model.addAttribute("informes", historiaClinica);
         return "historia/listado";
+    }
+
+    @GetMapping("/informe-detalle/{dni}")
+    public String mostrarListadoInformesDeSesionXPaciente(Model model, @PathVariable Integer dni) {
+        Paciente paciente;
+        List<Sesion> sesions;
+
+        if(dni > 0){
+            paciente = servicPaciente.getPacienteByDni(dni);
+            sesions = sesionService.findSesionByPacienteId(dni);
+        }else{
+            return "redirect:/historia-clinica";
+        }
+
+        model.addAttribute("paciente",paciente);
+        model.addAttribute("sesions",sesions);
+        return "historia/detalle";
     }
 }
