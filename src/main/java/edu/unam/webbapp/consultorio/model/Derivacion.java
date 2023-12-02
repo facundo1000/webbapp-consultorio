4c759aba602f2f75abb42c06215238ee2861afa7
facundo1000 <martinez.facundo85@gmail.com>
2023-12-02 18:08:11 -0300

    add: clase derivacion

---
+++ src/main/java/edu/unam/webbapp/consultorio/model/Derivacion.java
@@ -0,0 +1,50 @@
+package edu.unam.webbapp.consultorio.model;
+
+import edu.unam.webbapp.consultorio.utils.TipoDocumento;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "derivacion")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Derivacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "derivacion_id")
+    private Integer idDerivacion;
+
+    @Column(name = "nombre_dest")
+    private String nombreDestinatario;
+
+    @Column(name = "apellido_dest")
+    private String apellidoDestinatario;
+
+    @Column(name = "tipo_doc_dest")
+    @Enumerated(EnumType.STRING)
+    private TipoDocumento tipoDocDestinatario;
+
+    @Column(name = "dni_dest")
+    private Integer dniDestinatario;
+
+    @Column(name = "clinica_dest")
+    private String clinicaDestinataria;
+
+    @Column(name = "tel_clinica_dest")
+    private String telClinicaDestinataria;
+
+    @Column(name = "direc_clinica_dest")
+    private String direccionClinicaDestinataria;
+
+    @ManyToOne
+    private Psicologo psicologoEmisor;
+
+    private String descripcion;
+
+    @OneToOne(mappedBy = "derivacion")
+    private Paciente paciente;
+}
