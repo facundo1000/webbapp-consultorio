4551374acca6550effda0fff9d7278b912a47e0c
facundo1000 <martinez.facundo85@gmail.com>
2023-12-03 20:01:55 -0300

    add: derivacion repository

---
+++ src/main/java/edu/unam/webbapp/consultorio/repository/DerivacionesRepository.java
@@ -0,0 +1,7 @@
+package edu.unam.webbapp.consultorio.repository;
+
+import edu.unam.webbapp.consultorio.model.Derivacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DerivacionesRepository extends JpaRepository<Derivacion, Integer> {
+}
