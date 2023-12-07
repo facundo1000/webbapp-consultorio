39d3e926d54285ad619872f1f41e322f146ed625
facundo1000 <martinez.facundo85@gmail.com>
2023-12-07 10:31:08 -0300

    change: method PostMapping

--- src/main/java/edu/unam/webbapp/consultorio/controllers/DerivacionController.java
+++ src/main/java/edu/unam/webbapp/consultorio/controllers/DerivacionController.java
@@ -5,16 +5,18 @@ import edu.unam.webbapp.consultorio.model.Paciente;
 import edu.unam.webbapp.consultorio.model.Psicologo;
 import edu.unam.webbapp.consultorio.services.DerivacionesService;
 import edu.unam.webbapp.consultorio.services.PersonaService;
-import edu.unam.webbapp.consultorio.utils.EstadoCivil;
-import edu.unam.webbapp.consultorio.utils.Sexo;
+import edu.unam.webbapp.consultorio.services.impl.PacienteServiceImpl;
+import edu.unam.webbapp.consultorio.services.impl.PsicologoServiceImpl;
 import edu.unam.webbapp.consultorio.utils.TipoDocumento;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
@@ -23,66 +25,50 @@ import org.springframework.web.bind.support.SessionStatus;
 public class DerivacionController {
 
     private final DerivacionesService service;
-    private final PersonaService<Psicologo> psicoService;
-    private final PersonaService<Paciente> pacienteService;
+    private final PsicologoServiceImpl psicoService;
+    private final PacienteServiceImpl pacienteService;
 
     @GetMapping("/abm-derivaciones")
     public String crearDerivaciones(Model model) {
-        model.addAttribute("psicos",psicoService.findAll());
-        model.addAttribute("pacientes",pacienteService.findAll());
+        model.addAttribute("psicos", psicoService.findAll());
+        model.addAttribute("pacientes", pacienteService.findAll());
         model.addAttribute("derivaciones", service.findAll());
         model.addAttribute("tipoDoc", TipoDocumento.values());
         model.addAttribute("derivacion", new Derivacion());
         return "derivacion/abmDerivacion";
     }
 
+    //TODO: agregar @RequestParam para psicologo y paciente
+
     @PostMapping("/abm-derivaciones")
-    public String guardarDerivacion(@Valid Derivacion derivacion, BindingResult result, Model model, SessionStatus status) {
+    public String guardarDerivacion(@Valid Derivacion derivacion,
+                                    BindingResult result,
+                                    Model model,
+                                    SessionStatus status ,
+                                    @RequestParam("psicologo") Integer psicoId,
+                                    @RequestParam("paciente") Integer pacienteId) {
 
         if (result.hasErrors()) {
-            model.addAttribute("psicos",psicoService.findAll());
-            model.addAttribute("pacientes",pacienteService.findAll());
+            model.addAttribute("psicos", psicoService.findAll());
+            model.addAttribute("pacientes", pacienteService.findAll());
             model.addAttribute("derivaciones", service.findAll());
             model.addAttribute("tipoDoc", TipoDocumento.values());
             model.addAttribute("derivacion", new Derivacion());
             model.addAttribute("derivacion", derivacion);
         } else {
-            return "redirect:/abm-derivaciones";
+
+            return "derivacion/abmDerivacion";
         }
+
+
+        Psicologo psicologo = psicoService.getByDni(psicoId);
+        Paciente paciente = pacienteService.getPacienteByDni(pacienteId);
+
+        derivacion.setPaciente(paciente);
+        derivacion.setPsicologoEmisor(psicologo);
         service.save(derivacion);
         status.setComplete();
-        return "derivacion/abmDerivacion";
-    }
-
-    @GetMapping("/abm-derivaciones/{id}")
-    public String editar(@RequestParam(name = "page", defaultValue = "0") int page, @PathVariable("id") Integer id, Model model) {
-        Derivacion derivacion;
-//        int pageSize = 2;
-//        Page<Derivacion> pageData = service.getPageData(page,pageSize);
-
-        if (id > 0) {
-            derivacion = service.findById(id);
-            model.addAttribute("psicos",psicoService.findAll());
-            model.addAttribute("pacientes",pacienteService.findAll());
-            model.addAttribute("derivaciones", service.findAll());
-            model.addAttribute("tipoDoc", TipoDocumento.values());
-            model.addAttribute("derivacion", derivacion);
-//           model.addAttribute("pageData",pageData);
-        } else {
-            return "redirect:/abm-derivaciones";
-        }
-        model.addAttribute("derivacion", derivacion);
-        return "derivacion/abmDerivacion";
-    }
-
-
-
-
-    @DeleteMapping("/eliminar-derivaciones/{id}")
-    public String eliminar(@PathVariable("id") Integer id) {
-        if (id > 0) {
-            service.deleteById(id);
-        }
         return "redirect:/abm-derivaciones";
     }
+
 }
